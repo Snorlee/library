b996ac1e528a2e08c4a2c737fe6ec73629d9669c
Snorlee <brianlee132@gmail.com>
2020-07-07 19:46:15 -0400

    New commit for what I planned

---
+++ models/author.js
@@ -0,0 +1,82 @@
+var mongoose = require('mongoose');
+
+const moment = require('moment');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, maxlength: 100},
+    family_name: {type: String, required: true, maxlength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+
+// To avoid errors in cases where an author does not have either a family name or first name
+// We want to make sure we handle the exception by returning an empty string for that case
+
+  var fullname = '';
+  if (this.first_name && this.family_name) {
+    fullname = this.family_name + ', ' + this.first_name
+  }
+  if (!this.first_name || !this.family_name) {
+    fullname = '';
+  }
+
+  return fullname;
+});
+
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  let life = this.date_of_birth ? moment.utc(this.date_of_birth).format('MMMM Do, YYYY').toString() : 'Unknown';
+  life += " - "
+  life += this.date_of_death ? moment.utc(this.date_of_death).format('MMMM Do, YYYY').toString() : 'Unknown';
+  return life
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+// Virtual for author's date of birth formatted to YYYY-MM-DD
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+  return moment.utc(this.date_of_birth).format('YYYY-MM-DD');
+});
+
+// Virtual for author's date of death formatted to YYYY-MM-DD
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+  return moment.utc(this.date_of_death).format('YYYY-MM-DD');
+});
+
+// Virtual for author's date of birth formatted to MMMM Do, YYYY
+AuthorSchema
+.virtual('date_of_birth_formats')
+.get(function () {
+  return moment.utc(this.date_of_birth).format('MMMM Do, YYYY');
+});
+
+// Virtual for author's date of death formatted to MMMM Do, YYYY
+AuthorSchema
+.virtual('date_of_death_formats')
+.get(function () {
+  return moment.utc(this.date_of_death).format('MMMM Do, YYYY');
+});
+
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
